a468c24dfb793e066e00b0babcf36101a1aa0e07
CANADIABANK\sotheavuth.nguon <sotheavuth.nguon@yahoo.com>
2022-06-28 17:15:03 +0700

    Added icon box and notification box

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_food_recipe/theme/color.dart';
+import 'package:flutter_food_recipe/widgets/icon_box.dart';
+import 'package:flutter_food_recipe/widgets/notification_box.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -30,7 +33,15 @@ class _HomeScreenState extends State<HomeScreen> {
         SizedBox(
           width: 34,
           height: 34,
-        )
+          child: IconBox(
+            child: SvgPicture.asset('assets/icons/menu1.svg'),
+            radius: 15,
+            bgColor: appBgColor,
+          ),
+        ),
+        const NotificationBox(
+          notifiedNumber: 1,
+        ),
       ],
     );
   }
--- lib/widgets/icon_box.dart
+++ lib/widgets/icon_box.dart
@@ -1,1 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_food_recipe/theme/color.dart';
+
+class IconBox extends StatelessWidget {
+  const IconBox({
+    Key? key,
+    required this.child,
+    this.borderColor = Colors.transparent,
+    this.bgColor = Colors.white,
+    this.radius = 50,
+    this.padding = 5,
+    this.onTap,
+    this.isShadow = true,
+  }) : super(key: key);
+
+  final Widget child;
+  final Color borderColor;
+  final Color bgColor;
+  final double radius;
+  final double padding;
+  final GestureTapCallback? onTap;
+  final bool isShadow;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.all(padding),
+        decoration: BoxDecoration(
+          color: bgColor,
+          borderRadius: BorderRadius.circular(radius),
+          border: Border.all(color: borderColor),
+          boxShadow: [
+            if (isShadow)
+              BoxShadow(
+                color: shadowColor.withOpacity(0.1),
+                spreadRadius: 1,
+                blurRadius: 1,
+                offset: const Offset(0, 1),
+              ),
+          ],
+        ),
+        child: child,
+      ),
+    );
+  }
+}
